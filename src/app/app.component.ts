639faaa8d76efc3836cf58ff094b86f2eacadd45
herve <rv_changue@hotmail.com>
2021-12-11 13:30:17 -1000

    add task dialog

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,9 @@
+import { CdkDragDrop, transferArrayItem } from '@angular/cdk/drag-drop';
 import { Component } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { TaskDialogComponent } from './task-dialog/task-dialog.component';
+
+import { Task } from './task/task';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +12,64 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'kanban-fire';
+
+  todo: Task[] = [
+    {
+      title: 'Buy milk',
+      description: 'Go to the store and buy milk'
+    },
+    {
+      title: 'Create a Kanban app',
+      description: 'Using Firebase and Angular create a Kanban app!'
+    }
+  ];
+  inProgress: Task[] = [];
+  done: Task[] = [];
+  
+  constructor(private dialog: MatDialog) {}
+
+  newTask(): void {
+    const dialogRef = this.dialog.open(TaskDialogComponent, {
+      width: '270px',
+      data: {
+        task: {},
+      },
+    });
+    dialogRef
+      .afterClosed()
+      .subscribe((result: TaskDialogResult|undefined) => {
+        if (!result) {
+          return;
+        }
+        this.todo.push(result.task);
+      });
+  }
+
+  editTask(list: string, task: Task): void {}
+
+  drop(event: CdkDragDrop<Task[]>): void {
+    if (event.previousContainer === event.container) {
+      return;
+    }
+    if (!event.container.data || !event.previousContainer.data) {
+      return;
+    }
+    transferArrayItem(
+      event.previousContainer.data,
+      event.container.data,
+      event.previousIndex,
+      event.currentIndex
+    );
+  }
 }
+
+export interface TaskDialogData {
+  task: Partial<Task>;
+  enableDelete: boolean;
+}
+
+export interface TaskDialogResult {
+  task: Task;
+  delete?: boolean;
+}
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,18 +6,30 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { TaskComponent } from './task/task.component';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatButtonModule } from '@angular/material/button';
+import { DragDropModule } from '@angular/cdk/drag-drop';
+import { TaskDialogComponent } from './task-dialog/task-dialog.component';
+import { MatInputModule } from '@angular/material/input';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TaskComponent
+    TaskComponent,
+    TaskDialogComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     MatToolbarModule,
     MatCardModule,
-    MatIconModule
+    MatIconModule,
+    DragDropModule,
+    MatButtonModule,
+    MatDialogModule,
+    MatInputModule,
+    FormsModule
 
   ],
   providers: [],
