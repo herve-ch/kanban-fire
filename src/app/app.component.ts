4e5ca94985da341a6ab151109e514d83f66dcad9
herve <rv_changue@hotmail.com>
2021-12-12 16:58:20 -1000

    amelioration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,16 +3,24 @@ import { Component } from '@angular/core';
 import { Firestore, collectionData, collection, CollectionReference, query, runTransaction, doc, deleteDoc, addDoc } from '@angular/fire/firestore';
 import { MatDialog } from '@angular/material/dialog';
 import { updateDoc } from 'firebase/firestore';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, Observable } from 'rxjs';
 import { TaskDialogComponent } from './task-dialog/task-dialog.component';
 import { Task } from './task/task';
 
+const getObservable = (collection: Observable<Task[]>) => {
+  const subject = new BehaviorSubject<Task[]>([]);
+  collection.subscribe((val: Task[]) => {
+    subject.next(val);
+  });
+  return subject;
+};
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'kanban-fire';
 
@@ -36,13 +44,13 @@ export class AppComponent {
   constructor(private dialog: MatDialog, private firestore: Firestore) {
 
     const todoRef = collection(firestore, 'todo');
-    this.todo = collectionData(todoRef, { idField: 'id' }) as Observable<Task[]>;
+    this.todo = getObservable(collectionData(todoRef, { idField: 'id' }) as Observable<Task[]>);
 
     const inProgressRef = collection(firestore, 'inProgress');
-    this.inProgress = collectionData(inProgressRef, { idField: 'id' }) as Observable<Task[]>;
+    this.inProgress = getObservable(collectionData(inProgressRef, { idField: 'id' }) as Observable<Task[]>);
 
     const doneRef = collection(firestore, 'done');
-    this.done = collectionData(doneRef, { idField: 'id' }) as Observable<Task[]>;
+    this.done = getObservable(collectionData(doneRef, { idField: 'id' }) as Observable<Task[]>);
   }
 
   newTask(): void {
@@ -65,6 +73,7 @@ export class AppComponent {
         }
       });
   }
+
 
   editTask(list: 'done' | 'todo' | 'inProgress', task: Task): void {
     const dialogRef = this.dialog.open(TaskDialogComponent, {
